0d54125d0ffed568753e6cdf79affb1aeece836a
Michał Zieliński <michal@zielinscy.org.pl>
2013-08-27 15:51:49 +0200

    store user data in nodecache

--- domains.py
+++ domains.py
@@ -3,7 +3,7 @@ import graveldb
 import gravelrpc
 import cmd_util
 
-PATH = '/gravel/system/node'
+PATH = '/gravel/system/nodecache'
 
 class Domain(graveldb.Table('domains', PATH)):
     default = dict(owner=None, port=None, forward=None)
