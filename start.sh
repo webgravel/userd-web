85d60567cfe3743f77d644f6e211655bee2f092a
Michał Zieliński <michal@zielinscy.org.pl>
2013-09-23 23:30:35 +0200

    add guard to node.js server (as it dies)

--- start.sh
+++ start.sh
@@ -2,4 +2,18 @@ echo "proxy ALL = (ALL) NOPASSWD: /usr/local/bin/gravel-web-gettarget" > /etc/su
 chmod 440 /etc/sudoers.d/userd-proxy
 export NODE_PATH=/gravel/pkg/bouncy
 echo "running node route.js"
-exec node route.js
+
+quitfun() {
+    echo killing node
+    kill $pid
+    exit 0
+}
+
+trap quitfun TERM
+
+while true; do
+    node route.js & pid=$!
+    wait $pid
+    echo "node died unexpectly"
+    sleep 3
+done
